6f331f14665280b50595e54fba2877fad9afd79f
824EEdiego <diego20040328@gmail.com>
2023-12-04 02:56:46 +0800

    Test.cpp ready to modify

---
+++ Test.cpp
@@ -0,0 +1,40 @@
+Weapon Armory[6];//put all kind of weapons
+SelfArmory[1] -> get_Attack()
+
+//int main(){
+
+	InterateWithMoster(Engineer a0, Class_name of monster b0){
+		
+	}
+	
+	
+	//case left
+	Engineer::Engineer()
+			 :HP(200), Defense(10), Energy(120)
+	{
+		Xcoord = 0;
+		Ycoord = 0;
+		Velocity = 4;
+	}
+	
+	//case middle
+	Engineer::Engineer()
+			 :HP(150), Defense(40), Energy(170)
+	{
+		Xcoord = 0;
+		Ycoord = 0;
+		Velocity = 4;
+	}
+	//case right
+	Engineer::Engineer()
+			 :HP(100), Defense(100), Energy(200)
+	{
+		Xcoord = 0;
+		Ycoord = 0;
+		Velocity = 4;
+	}
+	void Engineer::
+	
+	
+	
+}
