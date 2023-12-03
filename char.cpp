d81b117bbe93f025aea6b17ca04609c2e5e5c6d8
824EEdiego <diego20040328@gmail.com>
2023-12-04 02:56:05 +0800

    char ready to modify

---
+++ char.cpp
@@ -0,0 +1,20 @@
+#include "char.h"
+EE_freshman::EE_freshman(){
+	HP = 200;
+	Defense = 20;
+	Energy = 200;
+}
+                                                                                                                                                                                                                                                                                       
+//EE_sophomore::
+
+namespace Wep
+{
+	//對每個在陣列中的武器初始化 
+}
+
+void EE_freshman::WeaponChoosing(){
+	
+}
+
+
+
